3a47621cdb70354fac0b101a898daf3ba0e15517
dawinkenwe <44124683+dawinkenwe@users.noreply.github.com>
2022-03-11 18:25:04 -0800

    Much Cleaner, fully functional. Last step is to make an .exe

--- rotation.py
+++ rotation.py
@@ -7,70 +7,95 @@ from playsound import playsound
 from tkinter import *
 from ctypes import *
 
-# arg 1 is for which desktop I believe.
-def setWallpaperWithCtypes(path):
-    ustr = create_string_buffer(path.encode('utf-8'))
-    ok = windll.user32.SystemParametersInfoA(win32con.SPI_SETDESKWALLPAPER, 0, ustr, 0)
+class Rotator:
+    def __init__(self):
+        self.oldbg = self.getCurrentBackground()
+        self.woo = os.getcwd()+"\sounds\woo.mp3"
+        self.newbg = os.getcwd()+"\\images\\rotated.jpg"
+        self.monitors = self.getMonitors()
+        
 
-def rotate(x):
-    rotation_val=win32con.DMDO_90
-    device = win32.EnumDisplayDevices(None,x)
-    dm = win32.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS)
-    if((dm.DisplayOrientation + rotation_val)%2==1):
-        dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth   
-    dm.DisplayOrientation = rotation_val
+    def getMonitors(self):
+        monitors = []
+        for i in range(10):
+            try:
+                device = win32.EnumDisplayDevices(None,i)
+                if device is None:
+                    break
+                else:
+                    monitors.append(i)
+            except Exception as e:
+                continue
+        return monitors
+        
 
-    win32.ChangeDisplaySettingsEx(device.DeviceName,dm)
+    def rotate(self, undo=False):
+        rotation_val=win32con.DMDO_90 if not undo else win32con.DMDO_DEFAULT
+        for x in self.monitors:
+            try:
+                device = win32.EnumDisplayDevices(None,x)
+                dm = win32.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS)
+                if((dm.DisplayOrientation + rotation_val)%2==1):
+                    dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth   
+                    dm.DisplayOrientation = rotation_val
 
-def playWoo():
-    playsound(os.getcwd()+"\sounds\woo.mp3")
-    
-def run():
-    displays = []
-    for i in range(10):
-        try:
-            device = win32.EnumDisplayDevices(None,i)
-            if device is None:
-                break
-            else:
-                displays.append(i)
-                print(device.DeviceName)
-                print(vars(device))
-        except Exception as e:
-            break
-    print("ok")
-    print(displays)
-    displays = [ x for x in range(10) if win32.EnumDisplayDevices(None,x) is not None ]
-    print(displays)
+                win32.ChangeDisplaySettingsEx(device.DeviceName,dm)
+            except Exception as e:
+                self.monitors.remove(x)
 
-def getCurrentBackground():
-    SPI_GETDESKWALLPAPER = 0x0073
-    dll = WinDLL('user32')
-    ubuf = create_unicode_buffer(200)
-    if dll.SystemParametersInfoW(SPI_GETDESKWALLPAPER,200,ubuf,0):
-        print(ubuf.value)
 
-def undo():
-    setWallpaperWithCtypes(r"C:\Users\dwink\AppData\Local\Microsoft\Windows\Themes\RoamedThemeFiles\DesktopBackground\1000 gates painting.jpg")
-    rotate()
+    def getCurrentBackground(self):
+        SPI_GETDESKWALLPAPER = 0x0073
+        dll = WinDLL('user32')
+        ubuf = create_unicode_buffer(200)
+        if dll.SystemParametersInfoW(SPI_GETDESKWALLPAPER,200,ubuf,0):
+            return ubuf.value
 
-def setwall():
-    setWallpaperWithCtypes(os.getcwd()+"\\images\\rotated.jpg")
+    def setWallpaperWithCtypes(self,path):
+        ustr = create_string_buffer(path.encode('utf-8'))
+        ok = windll.user32.SystemParametersInfoA(win32con.SPI_SETDESKWALLPAPER, 0, ustr, 0)
 
-def doGUI():
-    window = Tk()
-    window.title("StarFinder Character Creator")
-    window.geometry('350x200')
-    btn = Button(window, text="Click Me", command=playWoo)
-    btn.grid(column=1, row=0)
-    window.mainloop()
+    def do(self):
+        self.setWallpaperWithCtypes(self.newbg)
+        self.rotate()
+        playsound(self.woo)
+
+
+    def undo(self):
+        self.setWallpaperWithCtypes(self.oldbg)
+        self.rotate(True)
+
+class UI:
+    def __init__(self):
+        self.window = Tk()
+        self.butt = Button(self.window, text="Click Me", width=350, height=200)
+        self.rotator = Rotator()
+
+
+    def do(self):
+        self.rotator.do()
+        self.butt.configure(text="UNDO", command=self.undo)
+
+
+    def undo(self):
+        self.rotator.undo()
+        self.butt.configure(text="Click Me", command=self.do)
+        
+
+    def setup(self):
+        self.window.title("StarFinder Character Creator")
+        #width = self.window.winfo_screenwidth() 
+        #height = self.window.winfo_screenheight()
+        self.window.geometry("%dx%d" % (350, 200))
+        self.butt.configure(command=self.do)
+        self.butt.pack()
+
+
+    def run(self):
+        self.window.mainloop()
+
 
 if __name__ == "__main__":
-    # Rotate the screen
-    # Set the desktop background to be get rotated
-    # Play the woooo
-    #getCurrentBackground()
-
-    #doGUI()
-    setwall()
-    undo()
+    ui = UI()
+    ui.setup()
+    ui.run()
